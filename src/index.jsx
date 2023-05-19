cba49c87473a196cd7593b1961ca12a7bd29cfd3
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-05-19 16:10:34 -0400

    g

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./components";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
