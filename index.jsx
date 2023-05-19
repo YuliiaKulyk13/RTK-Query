a41bfb379041959aacb3009813a69a89ecb33932
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-05-19 15:45:49 -0400

    u

--- index.jsx
+++ index.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { App } from './components';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./components";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="RTK-Query">
     <Provider store={store}>
       <React.StrictMode>
         <App />
