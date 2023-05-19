974d69bf522e44afa7358c27cfb914126f26bb73
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-05-19 15:50:21 -0400

    u

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/RTK-Query/",
   server: {
     open: "https://github.com/YuliiaKulyk13/RTK-Query",
     port: 8000,
